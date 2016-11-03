2d9a884995ec860bbe3af6dbdd30f81c583b0dc2
Vikas Sundrani <sundrani.vikas@gmail.com>
2016-11-03 18:14:55 -0400

    heroku push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
